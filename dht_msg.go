c2421c236749519279729985a03ef89586c053f7
Martin Persson <martin_persson91@hotmail.com>
2016-10-06 13:34:32 +0200

    fixat i msg

--- dht_msg.go
+++ dht_msg.go
@@ -8,12 +8,16 @@ type Msg struct {
 	Src    string //från noden som kalla
 	Dst    string //destinationsadress
 	Bytes  []byte //transport funktionen, msg.Bytes
+	Adress string
+	Id	   string
 	Type   string // type of message thats is being sent
 }
 
 func message(t, origin, dst, src, key string, bytes []byte) *Msg {
 	msg := &Msg{}
 	msg.Type = t
+	msg.Adress = adress
+	msg.Id = id
 	msg.Origin = origin
 	msg.Src = src
 	msg.Dst = dst
@@ -25,6 +29,9 @@ func message(t, origin, dst, src, key string, bytes []byte) *Msg {
 func joinMessage(dst string) *Msg {
 	msg := &Msg{}
 	msg.Type = "addToRing"
+	msg.Adress = adress
+	msg.Id = id
+	msg.Origin = origin
 	msg.Src = ""
 	msg.Dst = dst
 	msg.Bytes = nil
@@ -35,6 +42,8 @@ func joinMessage(dst string) *Msg {
 func printMessage(origin, dst string) *Msg {
 	msg := &Msg{}
 	msg.Type = "printRing"
+	msg.Adress = adress
+	msg.Id = id
 	msg.Origin = origin
 	msg.Src = ""
 	msg.Dst = dst
@@ -46,6 +55,8 @@ func printMessage(origin, dst string) *Msg {
 func notifyMessage(src, dst string) *Msg {
 	msg := &Msg{}
 	msg.Type = "notify"
+	msg.Adress = adress
+	msg.Id = id
 	msg.Origin = ""
 	msg.Key = ""
 	msg.Src = src
@@ -57,8 +68,22 @@ func notifyMessage(src, dst string) *Msg {
 func getNodeMessage(src, dst string) *Msg {
 	msg := &Msg{}
 	msg.Type = "pred"
+	msg.Adress = adress
+	msg.Id = id
+	msg.Origin = origin
 	msg.Src = src
 	msg.Dst = dst
 	msg.Bytes = nil
 	return msg
 }
+
+func responseMsg(src, dst, adress, id string) *Msg {
+	msg := &Msg{}
+	msg.Type = "response"
+	msg.Adress = adress
+	msg.Id = id
+	msg.Origin = origin
+	msg.Src = src
+	msg.Dst = dst
+	msg.Bytes = nil
+	return msg}
