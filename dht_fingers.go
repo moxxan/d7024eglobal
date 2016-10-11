93c3205cbcb33959de3a548c84c1f64888fa4547
Niklas Brynolf <niklas.brynolf@gmail.com>
2016-10-11 14:04:34 +0200

    removed dead code

--- dht_fingers.go
+++ dht_fingers.go
@@ -39,25 +39,6 @@ func (node *DHTNode) fingerTimer() {
 	}
 }
 
-/*
-func init_finger_table(n *DHTNode) [bits]*DHTNode{
-	var templist [bits]*DHTNode
-	for i := 0; i < bits; i++ {
-		x,_ := hex.DecodeString(n.nodeId) //func DecodeString(s string) ([]byte, error)
-		y, _ := calcFinger(x, (i+1), bits) // returnerar (string, []byte)
-		if y == "" {
-			y = "00"
-		} else{}
-		//fmt.Println("FINGER", i+1, "POINTS ON NODE", y)
-		succ := n.lookup(y)  //ändra ej, vanlig lookup
-		// calcFinger(n []byte, k int, m int) (string, []byte) {
-		templist[i] = succ
-
-	}
-return templist
-}
-*/
-
 func (node *DHTNode) updateFingers() {
 	nodeAdress := node.contact.ip + ":" + node.contact.port
 	for i := 0; i < bits; i++ {
@@ -89,12 +70,3 @@ func (node *DHTNode) updateFingers() {
 		}
 	}
 }
-
-/*if y == node.fingers.nodefingerlist[i].nodeId {
-		} else {
-			a := node.lookup(y)
-			node.fingers.nodefingerlist[i] = a
-		}
-	}
-	return node.fingers.nodefingerlist
-}*/
